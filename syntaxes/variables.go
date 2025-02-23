aea398c011dd814750b8fac7927aa4cdd867ff85
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2025-02-23 13:30:13 +0530

    variables

--- syntaxes/variables.go
+++ syntaxes/variables.go
@@ -3,8 +3,21 @@ package main
 import "fmt"
 
 func main(){
-
+  
+  //Method 1
   var a = "initial"
-  fmt.Println(a)
+  
+  //Method 2
+  var b,c int = 1,2
 
+  //Method 3 ( gets zeroed value which is 0 in the case of int )
+  var d int
+
+  //Method 4 ( only available inside functions )
+  e := "string"
+
+  //Constants
+  const f int = 10
+
+  fmt.Println(a,b,c,d,e,f+10)
 }
