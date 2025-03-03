dbf48a20fabe3358f8be2b6d35aa248a8a45ace4
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2025-03-04 04:56:13 +0530

    RSSaggregator

--- RSSaggregator/main.go
+++ RSSaggregator/main.go
@@ -7,12 +7,32 @@ import (
 	"github.com/gofiber/fiber/v3"
 )
 
+type User struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+var users = []User{}
+var nextID = 1
+
 func main() {
 	fmt.Println("Hello World")
 	app := fiber.New()
 
 	app.Get("/", func(c fiber.Ctx) error {
-		return c.Status(200).JSON("Hello, World 👋!")
+		return c.Status(200).JSON(users)
+	})
+
+	app.Post("/users", func(c fiber.Ctx) error {
+		user := new(User)
+		if err := c.Bind().JSON(user); err != nil {
+			return c.Status(400).JSON(fiber.Map{"error": "Invalid request"})
+		}
+
+		user.ID = nextID
+		nextID++
+		users = append(users, *user)
+		return c.Status(200).JSON(user)
 	})
 
 	log.Fatal(app.Listen(":3000"))
