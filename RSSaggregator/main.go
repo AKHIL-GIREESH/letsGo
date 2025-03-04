7c857a7e93532cde71cd3092040450429762e717
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2025-03-04 05:43:03 +0530

    RSSaggregator

--- RSSaggregator/main.go
+++ RSSaggregator/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"strconv"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/joho/godotenv"
 )
 
 type User struct {
@@ -18,6 +21,11 @@ var nextID = 1
 func main() {
 	fmt.Println("Hello World")
 	app := fiber.New()
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	app.Get("/", func(c fiber.Ctx) error {
 		return c.Status(200).JSON(users)
@@ -35,5 +43,37 @@ func main() {
 		return c.Status(200).JSON(user)
 	})
 
-	log.Fatal(app.Listen(":3000"))
+	app.Patch("/users/:id", func(c fiber.Ctx) error {
+		idStr := c.Params("id")
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			return c.Status(400).JSON(fiber.Map{"error": "Invalid request"})
+		}
+
+		var user *User = nil
+
+		for i := range users {
+			if users[i].ID == id {
+				user = &users[i]
+				break
+			}
+		}
+
+		if user == nil {
+			return c.Status(404).JSON(fiber.Map{"error": "User not found"})
+		}
+
+		var updates User
+		if err := c.Bind().JSON(&updates); err != nil {
+			return c.Status(400).JSON(fiber.Map{"error": "Invalid request body"})
+		}
+
+		if updates.Name != "" {
+			user.Name = updates.Name
+		}
+
+		return c.Status(200).JSON(user)
+	})
+
+	log.Fatal(app.Listen(":" + os.Getenv("PORT")))
 }
